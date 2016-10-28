355188021bf2248537d0ee570e2ae215d646f1b8
haroldfolk <haroldfolk@gmail.com>
2016-10-28 01:20:19 -0400

    COMmit de solucion de comfig db ingore 3

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=fotoweb',
     'username' => 'root',
-    'password' => 'yohanita',
+    'password' => '',
     'charset' => 'utf8',
 ];
